bc25407f06ccc746f679591aa66bb49e918a1a3b
Santo Prestandrea <131533276+santoprestandrea@users.noreply.github.com>
2026-01-06 18:36:39 +0100

    Update HospitalVisitsApplication.java

--- hospital-visits/src/main/java/com/hospital/hospital_visits/HospitalVisitsApplication.java
+++ hospital-visits/src/main/java/com/hospital/hospital_visits/HospitalVisitsApplication.java
@@ -1,13 +1,34 @@
 package com.hospital.hospital_visits;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class HospitalVisitsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalVisitsApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        // control if the file exists in the right directory
+        Path envPath = Path.of("example.env").toAbsolutePath();
+        System.out.println("Looking for example.env at: " + envPath);
+
+
+        Dotenv dotenv = Dotenv.configure()
+                .filename("example.env")
+                .load();
+
+        dotenv.entries().forEach(e -> {
+            if (System.getenv(e.getKey()) == null && System.getProperty(e.getKey()) == null) {
+                System.setProperty(e.getKey(), e.getValue());
+            }
+        });
+
+
+        System.out.println("JWT_SECRET loaded? " + (System.getProperty("JWT_SECRET") != null));
+
+        SpringApplication.run(HospitalVisitsApplication.class, args);
+    }
 }
